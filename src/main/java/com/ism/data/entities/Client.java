bc3543fd2c15f6346eb8d24d19659a0cbe0814a7
douvewane <douvewane85@gmail.com>
2024-09-25 11:41:07 +0000

    Mis en place des Entity

--- src/main/java/com/ism/data/entities/Client.java
+++ src/main/java/com/ism/data/entities/Client.java
@@ -1,5 +1,13 @@
 package com.ism.data.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,12 +17,20 @@ import lombok.ToString;
 @Setter
 @ToString
 @EqualsAndHashCode()
+@Entity
+@Table(name = "clients")
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @Column(length = 25, unique = true)
     private String surname;
+    @Column(length = 11, unique = true)
     private String telephone;
+    @Column(length = 255, unique = false)
     private String adresse;
 
     // Navigabilite
+    @OneToOne
     private User user;
 }
--- src/main/java/com/ism/data/entities/User.java
+++ src/main/java/com/ism/data/entities/User.java
@@ -1,20 +1,42 @@
 package com.ism.data.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ColumnDefault;
+
 import com.ism.data.enums.RoleEnum;
 
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @Column(length = 25, unique = true)
     private String login;
     private String prenom;
     private String nom;
     private String password;
+    @Enumerated(EnumType.STRING)
     private RoleEnum role;
+    @ColumnDefault(value = "true")
     private boolean etat;
 
     // Navigabilite
+    @OneToOne
+    @JoinColumn(name = "clients_id", nullable = true)
     private Client client;
 
 }
